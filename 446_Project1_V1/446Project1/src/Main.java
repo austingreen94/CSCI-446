9965353f3b16568126606a1b69235cbb43d06b67
vanDan5 <rohmannjk@yahoo.com>
2016-09-22 20:35:00 -0600

    Add files via upload

--- 446_Project1_V1/446Project1/src/Main.java
+++ 446_Project1_V1/446Project1/src/Main.java
@@ -1,4 +1,5 @@
 
+import java.util.concurrent.TimeUnit;
 import javax.swing.JFrame;
 
 /**
@@ -9,7 +10,7 @@ import javax.swing.JFrame;
  */
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         GraphGen Graph = new GraphGen();
         Graph.createNodes();
         Graph.setEdges();
@@ -22,16 +23,11 @@ public class Main {
         application.add(panel);
         application.setSize (Graph.squareDimension*50+30, Graph.squareDimension*50+50);
         application.setVisible(true);
+        TimeUnit.SECONDS.sleep(1);
         
-        long i=0;
-        while(i<100000){
-            i++;
-            System.out.println(i);
-        }
-        
-        MinConflicts minConflicts = new MinConflicts(Graph.finishedPoints);
+        MinConflicts algorithms = new MinConflicts(Graph.finishedPoints);
         // Max number of runs = 10000 & 4 colors being used
-        int minConflictSuccess = minConflicts.minConflicts(10000, 4);
+        int minConflictSuccess = algorithms.minConflicts(10000, 4);
         if(minConflictSuccess != -1){
             System.out.println("Number of Decisions of Min_Conflicts = " + minConflictSuccess);
         } else {
@@ -44,11 +40,49 @@ public class Main {
         app2.setSize (Graph.squareDimension*50+30, Graph.squareDimension*50+50);
         app2.setVisible(true);
         // Reset graph
-        i=0;
-        while(i<100000){
-            i++;
-            System.out.println(i);
-        }
-        minConflicts.resetGraph();
+        TimeUnit.SECONDS.sleep(1);
+        algorithms.resetGraph();
+        
+        //initialize backtracking
+        Backtracking back = new Backtracking(Graph.finishedPoints);
+        int decisions = back.init(4); //4 color types
+        System.out.println("Number of Decisions of Simple Backtracking = " + decisions);
+        //make new frame for backtracking
+        JFrame app3 = new JFrame("Backtracking");
+        app3.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        app3.add(panel);
+        app3.setSize (Graph.squareDimension*50+30, Graph.squareDimension*50+50);
+        app3.setVisible(true);
+        //reset graph
+        TimeUnit.SECONDS.sleep(1);
+        algorithms.resetGraph();
+        
+        //initialize backtrackingFC
+        BacktrackingFC backFC = new BacktrackingFC(Graph.finishedPoints);
+        decisions = backFC.init(4); //4 color types
+        System.out.println("Number of Decisions of Backtracking with Forward Checking= " + decisions);
+        //make new frame for backtracking
+        JFrame appBackFC = new JFrame("Backtracking with Forward Check");
+        appBackFC.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        appBackFC.add(panel);
+        appBackFC.setSize (Graph.squareDimension*50+30, Graph.squareDimension*50+50);
+        appBackFC.setVisible(true);
+        //reset graph
+        TimeUnit.SECONDS.sleep(1);
+        algorithms.resetGraph();
+        
+        //initialize backtrackingMAC
+        BacktrackingMAC backMAC = new BacktrackingMAC(Graph.finishedPoints);
+        decisions = backMAC.init(4); //4 color types
+        System.out.println("Number of Decisions of Backtracking with Constant Propagation= " + decisions);
+        //make new frame for backtracking
+        JFrame appBackMAC = new JFrame("Backtracking with Constant Propagation (MAC)");
+        appBackMAC.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        appBackMAC.add(panel);
+        appBackMAC.setSize (Graph.squareDimension*50+30, Graph.squareDimension*50+50);
+        appBackMAC.setVisible(true);
+        //reset graph
+        TimeUnit.SECONDS.sleep(1);
+        //algorithms.resetGraph();
     }
 }
