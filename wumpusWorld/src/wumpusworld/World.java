12b67d585646be30f1d12490a6c98606dd06589d
Brandon Busby <manamini@users.noreply.github.com>
2016-10-18 23:13:46 -0600

    Add files via upload
    World makes barrier of walls around the world.
    World goes node by node with a 10% chance to create either a wall, wumpus, hole, or gold.
    World will only make one gold drop and at least one wumpus.

--- wumpusWorld/src/wumpusworld/World.java
+++ wumpusWorld/src/wumpusworld/World.java
@@ -7,103 +7,86 @@
 package wumpusworld;
 
 import java.util.Random;
+
 /**
  *
  * @author b53t457
  */
 public class World {
+
     //Enter N for size of world
     int n = 5;
-    node[][] world = new node[n+2][n+2];
+    node[][] world = new node[n + 2][n + 2];
     Random random = new Random();
-    public void startworld()
-    {
-        for(int i = 0; i<n+2; i++)
-        {
-            for(int j = 0; j<n+2; j++)
-            {
-                
+    boolean goldy = false;
+    boolean wumpy = false;
+    int min = 0;
+    int max = n+1;
+
+    public void startworld() {
+        for (int i = 0; i < n + 2; i++) {
+            for (int j = 0; j < n + 2; j++) {
+
                 world[i][j] = new node();
             }
         }
     }
-    public void hole()
-    {
-        int holex = random.nextInt(n)+1;
-        int holey = random.nextInt(n)+1;
-        
-        //world[holex][holey].gold = true;
-        if(world[holex][holey].gold == false && world[holex][holey].wumpus == false && world[holex][holey].wall == false)
+    public void buildworld() {
+        for(int i = 0; i<world.length; i++)
         {
-        System.out.println("hole location is : "+holex + " " + holey);
-        world[holex][holey].hole = true;
-        //System.out.println(world[holex][holey].hole);
+            world[min][i].wall = true;
+            world[max][i].wall = true;
+            world[i][min].wall = true;
+            world[i][max].wall = true;
         }
-        else
+        
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < n; j++) {
+                int bder = random.nextInt(100);
+                if (bder < 10 && world[i][j].gold == false && world[i][j].hole == false && world[i][j].wumpus == false && world[i][j].wall == false) {
+                    world[i][j].hole = true;
+                } else if (bder >= 10 && bder < 20 && world[i][j].gold == false && world[i][j].hole == false && world[i][j].wumpus == false && world[i][j].wall == false) {
+                    world[i][j].wumpus = true;
+                    wumpy = true;
+                } else if (bder >= 20 && bder < 30 && world[i][j].gold == false && world[i][j].hole == false && world[i][j].wumpus == false && world[i][j].wall == false) {
+                    world[i][j].wall = true;
+                } else if (bder >= 30 && bder < 40 && goldy == false && world[i][j].gold == false && world[i][j].hole == false && world[i][j].wumpus == false && world[i][j].wall == false) {
+                    world[i][j].gold = true;
+                    goldy = true;
+                }
+            }
+        }
+        if(goldy == false || wumpy == false)
         {
-            System.out.println("hole cannot be placed at : " + holex + " " + holey);
-            hole();
+            buildworld();
+        }
+        System.out.println("World is built");
+        for (int i = 0; i < n + 2; i++) {
+            for (int j = 0; j < n + 2; j++) {
+                if(world[i][j].gold== true)
+                {
+                    System.out.print("[G]");
+                }
+                else if(world[i][j].hole== true)
+                {
+                    System.out.print("[H]");
+                }
+                else if(world[i][j].wumpus== true)
+                {
+                    System.out.print("[W]");
+                }
+                else if(world[i][j].wall== true)
+                {
+                    System.out.print("[O]");
+                }
+                else
+                {
+                    System.out.print("[X]");
+                }
+                
+                //System.out.print("[" +(world[i][j].gold) +", " + (world[i][j].hole) +", " + (world[i][j].wall) +", " + (world[i][j].wumpus) + "]");
+            }
+            System.out.println();
         }
     }
-    public void wumpus()
-    {
-                int wump_x = random.nextInt(n)+1;
-        int wump_y = random.nextInt(n)+1;
-        
-        if(world[wump_x][wump_y].gold == false && world[wump_x][wump_y].hole == false && world[wump_x][wump_y].wall == false)
-        {
-            System.out.println("wumpus location is : "+wump_x + " " + wump_y);
-            world[wump_x][wump_y].wumpus = true;
-            //System.out.println(world[wump_x][wump_y].wumpus);
-        }    
-        else
-        {
-            System.out.println("wumpus cannot be placed at : " +wump_x + " " + wump_y);
-            wumpus();
-        }
-    }
-    public void wall()
-    {
-                int obsx = random.nextInt(n)+1;
-        int obsy = random.nextInt(n)+1;
-        
-        if(world[obsx][obsy].gold == false && world[obsx][obsy].wumpus == false && world[obsx][obsy].hole == false)
-        {
-            System.out.println("wall location is : "+ obsx + " " + obsy);
-            world[obsx][obsy].wall = true;
-            //System.out.println(world[obsx][obsy].wall);
-        }  
-        else
-        {
-            System.out.println("wall cannot be placed at : " + obsx + " " + obsy);
-            wall();
-        }
-    }
-    public void gold()
-    {
-        int goldx = random.nextInt(n)+1;
-        int goldy = random.nextInt(n)+1;
-        
-        if(world[goldx][goldy].hole == false && world[goldx][goldy].wumpus == false && world[goldx][goldy].wall == false)
-        {
-        System.out.println("gold location is : " +goldx + " " + goldy);    
-        world[goldx][goldy].gold = true;
-        //System.out.println(world[goldx][goldy].gold);
-        }
-        else
-        {
-            System.out.println("gold cannot be placed at : " +goldx + " " + goldy);
-            gold();
-        }
-    }
-    
-    public void buildworld()
-    {
-        hole();
-        wumpus();
-        wall();
-        gold();
-
-            System.out.println("World is built");
-        }
 }
